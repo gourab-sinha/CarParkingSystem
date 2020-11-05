e6cfb9ccae8a601066047fc287b12bf721453140
gourab-sinha <gourab19964u@gmail.com>
2020-11-05 10:29:55 +0530

    User class added

---
+++ src/users/User.java
@@ -0,0 +1,10 @@
+package users;
+
+public class User {
+    private String name;
+    private String Address;
+    public User(String name, String Address){
+        this.name=name;
+        this.Address=Address;
+    }
+}
